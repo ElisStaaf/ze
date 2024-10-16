64792aba85cdd09e75dcb1f6b3ef6a5deab820a7
eliss <staaf.elis@gmail.com>
2024-10-16 14:18:57 +0200

    So uhh... Source code comments, am i right?

--- ze.c
+++ ze.c
@@ -4,32 +4,32 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Function to list files and directories in the given path
+/*Function to list files and directories in the given path */
 void listFiles(const char *path) {
-    struct dirent *de;  // Pointer for directory entry
+    struct dirent *de;  /* Pointer for directory entry */
 
-    // Open the directory specified by the path
+    /* Open the directory specified by the path */
     DIR *dr = opendir(path);
 
     char red[]  = "\033[1;31m";
     char blue[] = "\033[1;34m";
 
-    if (dr == NULL) {  // Check if the directory could be opened
+    if (dr == NULL) {  /* Check if the directory could be opened */
         printf("%s[ERROR]: Could not open directory %s%s\n", red, path, blue);
         return;
     }
 
     printf("\nContents of %s:\n", path);
     printf("----------------------------\n");
-    // Read and print all files and directories within the directory
+    /* Read and print all files and directories within the directory */
     while ((de = readdir(dr)) != NULL)
         printf("%s\n", de->d_name);
 
-    // Close the directory
+    /* Close the directory */
     closedir(dr);
 }
 
-int main() {
+int main(int argc, char *argv[]) {
     int choice;
     char path[256];
     char filename[100];
@@ -43,7 +43,7 @@ int main() {
     char white[]  = "\033[1;37m";
 
     while (1) {
-        // Display the menu
+        /* Display the menu */
         printf("\n%s===================================\n", blue);
         printf("%s|       %sTHE ZE FILE EXPLORER%s      |\n", blue, yellow, blue);
         printf("%s===================================\n", blue);
@@ -56,32 +56,32 @@ int main() {
         printf("%s===================================\n", blue);
         printf("%s%%%s ", purple, blue);
         scanf("%d", &choice);
-        getchar(); // To consume the newline character after the choice
+        getchar(); /* To consume the newline character after the choice */
 
         switch (choice) {
             case 1:
-                // List files and directories in the given path
+                /* List files and directories in the given path */
                 printf("%s>%s ", purple, blue);
                 fgets(path, sizeof(path), stdin);
-                path[strcspn(path, "\n")] = 0; // Remove the newline character
+                path[strcspn(path, "\n")] = 0; /* Remove the newline character */
                 listFiles(path);
                 break;
 
             case 2:
-                // Change the current working directory
+                /* Change the current working directory */
                 printf("%s>%s ", purple, blue);
                 fgets(path, sizeof(path), stdin);
-                path[strcspn(path, "\n")] = 0; // Remove the newline character
+                path[strcspn(path, "\n")] = 0; /* Remove the newline character */
                 if (chdir(path) != 0) {
                     printf("%s[ERROR]: Could not change directory to %s%s\n", red, path, blue);
                 }
                 break;
 
             case 3:
-                // Create a new file
+                /* Create a new file */
                 printf("%s>%s ", purple, blue);
                 fgets(filename, sizeof(filename), stdin);
-                filename[strcspn(filename, "\n")] = 0; // Remove the newline character
+                filename[strcspn(filename, "\n")] = 0; /* Remove the newline character */
                 FILE *fp = fopen(filename, "w");
                 if (!fp) {
                     printf("%s[ERROR]: Could not create the file %s%s\n", red, filename, blue);
@@ -89,17 +89,17 @@ int main() {
                 break;
 
             case 4:
-                // Delete an existing file
+                /* Delete an existing file */
                 printf("%s>%s ", purple, blue);
                 fgets(filename, sizeof(filename), stdin);
-                filename[strcspn(filename, "\n")] = 0; // Remove the newline character
+                filename[strcspn(filename, "\n")] = 0; /* Remove the newline character */
                 if (remove(filename) != 0) {
                     printf("%s[ERROR]: File \"%s\" couldn't be deleted%s\n", red, filename, blue);
                 }
                 break;
 
             case 5:
-                // Run a custom command
+                /* Run a custom command */
                 printf("%s>%s ", purple, blue);
                 char cmd[256];
                 scanf("%s", &cmd);
@@ -107,7 +107,7 @@ int main() {
                 break;
 
             case 6:
-                // Exit the program
+                /* Exit the program */
                 exit(0);
 
             default:
